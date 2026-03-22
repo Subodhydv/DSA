e4ae3142989fca43bf933d5c671ca8dbbc6f9f03
Subodhydv <136544821+Subodhydv@users.noreply.github.com>
2026-03-22 21:31:47 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (24.42%) - LeetSync

---
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        int minVal = *min_element(nums.begin(), nums.end());
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+      return sum-n*minVal;
+        
+    }
+};
