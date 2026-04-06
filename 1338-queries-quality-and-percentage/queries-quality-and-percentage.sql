8165c1a69f752be960e875d8dbbe90dfcfe29954
Subodhydv <136544821+Subodhydv@users.noreply.github.com>
2026-04-06 13:06:26 +0530

    Time: 377 ms (26.66%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,6 +1,5 @@
-SELECT 
-    query_name,
-    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
-    ROUND(AVG(rating < 3) * 100, 2) AS poor_query_percentage
-FROM Queries
-GROUP BY query_name;
+select  query_name ,
+round(avg(rating*1.0/ position),2) as quality,
+round(avg(rating<3)*100,2) as poor_query_percentage
+from Queries
+group by  query_name ;
