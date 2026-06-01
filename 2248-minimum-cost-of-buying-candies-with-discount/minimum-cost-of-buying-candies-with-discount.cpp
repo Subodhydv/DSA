1887e774eb19d1f8b94c8c2ae7edbb170e898302
Subodhydv <136544821+Subodhydv@users.noreply.github.com>
2026-06-01 09:45:02 +0545

    Time: 4 ms (5.99%) | Memory: 16.8 MB (12.55%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        int n=cost.size();
+        int ans=0;
+        sort(cost.rbegin(),cost.rend());
+        for(int i=0;i<n;i++){
+          if ((i + 1) % 3 != 0) {
+                ans += cost[i];
+            }
+        }
+        return ans;
+    }
+};
