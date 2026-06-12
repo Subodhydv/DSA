8ec7c5a11fdd63b5cc0f91003495962fcca6e137
Subodhydv <136544821+Subodhydv@users.noreply.github.com>
2026-06-12 08:45:20 +0545

    Time: 0 ms (100.00%) | Memory: 95.3 MB (41.96%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,49 +1,48 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1,
-                                  vector<int>& nums2) {
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
 
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+        int n1 = a.size();
+        int n2 = b.size();
 
         if(n1 > n2)
-            return findMedianSortedArrays(nums2, nums1);
+            return findMedianSortedArrays(b, a);
 
         int low = 0;
         int high = n1;
 
-        while(low <= high){
+        int left = (n1 + n2 + 1) / 2;
 
-            int cut1 = low + (high-low)/2;
+        while(low <= high) {
 
-            int cut2 = (n1+n2+1)/2 - cut1;
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
 
-            int l1 = (cut1==0) ? INT_MIN : nums1[cut1-1];
-            int l2 = (cut2==0) ? INT_MIN : nums2[cut2-1];
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
 
-            int r1 = (cut1==n1) ? INT_MAX : nums1[cut1];
-            int r2 = (cut2==n2) ? INT_MAX : nums2[cut2];
+            if(mid1 < n1) r1 = a[mid1];
+            if(mid2 < n2) r2 = b[mid2];
 
-            if(l1 <= r2 && l2 <= r1){
+            if(mid1 - 1 >= 0) l1 = a[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = b[mid2 - 1];
 
-                if((n1+n2)%2==0){
+            if(l1 <= r2 && l2 <= r1) {
 
-                    return (max(l1,l2)+min(r1,r2))/2.0;
-                }
-                else{
+                if((n1 + n2) % 2 == 1)
+                    return max(l1, l2);
 
-                    return max(l1,l2);
-                }
+                return (double)(max(l1, l2) + min(r1, r2)) / 2.0;
             }
 
-            else if(l1 > r2){
-
-                high = cut1 - 1;
+            else if(l1 > r2) {
+                high = mid1 - 1;
             }
 
-            else{
-
-                low = cut1 + 1;
+            else {
+                low = mid1 + 1;
             }
         }
 
