bee0633fba317edc9b468e7ee2999f5c1d46e960
Subodhydv <136544821+Subodhydv@users.noreply.github.com>
2026-06-23 15:25:40 +0545

    Time: 8 ms (34.26%) | Memory: 19.6 MB (42.52%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool canFinish(int numCourses,
+                   vector<vector<int>>& prerequisites)
+    {
+        vector<int> adj[numCourses];
+
+        for(auto e : prerequisites)
+        {
+            int course = e[0];
+            int prereq = e[1];
+
+            adj[prereq].push_back(course);
+        }
+
+        vector<int> indegree(numCourses,0);
+
+        for(int i=0;i<numCourses;i++)
+        {
+            for(auto it : adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+
+        int cnt = 0;
+
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            cnt++;
+
+            for(auto it : adj[node])
+            {
+                indegree[it]--;
+
+                if(indegree[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+
+        return cnt == numCourses;
+    }
+};
