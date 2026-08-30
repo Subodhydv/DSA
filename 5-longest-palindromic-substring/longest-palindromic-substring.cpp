d712c385f4e683626da9534b53cf46173fa160e0
Subodhydv <136544821+Subodhydv@users.noreply.github.com>
2026-08-30 08:18:01 +0530

    Time: 7 ms (84.76%) | Memory: 9.2 MB (91.82%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,24 +1,35 @@
 class Solution {
-      void expand(string &s,int l,int r, int &start,int &maxlen){
-        while(l>=0 && r<s.size()&& s[l]==s[r]){
-            int len=r-l+1;
-            if(len>maxlen){
-                maxlen=len;
-                start=l;
-            }
-            l--;
-            r++;
-        }
-      };
 public:
     string longestPalindrome(string s) {
+        int n=s.size();
+        if(n<=1)return s;
         int start=0;
         int maxlen=1;
-        for(int i=0;i<s.size();i++){
-            expand(s,i,i,start,maxlen);
-            expand(s,i,i+1,start,maxlen);
+        for(int i=0;i<n;i++){
+            int left=i;
+            int right=i;
+            while(left>=0 && right<n && s[left]==s[right]){
+                int len=right-left+1;
+                if(len>maxlen){
+                    maxlen=len;
+                    start=left;
+                }
+                left--;
+                right++;
+            }
+            left=i;
+            right=i+1;
+            while(left>=0 && right<n && s[left]==s[right]){
+            int len=right-left+1;
+            if(len>maxlen){
+                maxlen=len;
+                start=left;
+            }
+            left--;
+            right++;
+            }
         }
-        return s.substr(start,maxlen);
+      return s.substr(start,maxlen);
         
     }
 };
