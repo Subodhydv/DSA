23bcf524aa84c7259d3c709af6fa41339db71e07
Subodhydv <136544821+Subodhydv@users.noreply.github.com>
2026-08-30 08:06:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (97.34%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+     int robrange(vector<int>& nums,int l, int r){
+        int prev2=0;
+        int prev1=0;
+        for(int i=l;i<=r;i++){
+            int take=prev2+nums[i];
+            int skip=prev1;
+            int cur=max(take,skip);
+            prev2=prev1;
+            prev1=cur;
+        }
+        return prev1;
+     }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+        int case1=robrange(nums,0,n-2);
+        int case2=robrange(nums,1,n-1);
+        return max(case1,case2);
+        
+    }
+};
